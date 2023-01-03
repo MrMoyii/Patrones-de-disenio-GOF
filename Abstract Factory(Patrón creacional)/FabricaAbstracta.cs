2e68d7204f194eba0283633a8bd4c1697fca7776
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-03 17:03:18 -0300

    implementacion de interface

---
+++ Abstract Factory(Patrón creacional)/FabricaAbstracta.cs
@@ -0,0 +1,15 @@
+﻿using Factory__Patrón_creacional_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Patrón_creacional_
+{
+    internal interface FabricaAbstracta
+    {
+        IConexion getBD(string motor);
+        IConexionREST getREST(string area);
+    }
+}
