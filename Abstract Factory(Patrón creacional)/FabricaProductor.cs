08a564d29537f4931bcd240f30b78f4c3d1547f4
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-03 17:56:00 -0300

    implementacion del patron

---
+++ Abstract Factory(Patrón creacional)/FabricaProductor.cs
@@ -0,0 +1,28 @@
+﻿using Factory__Patrón_creacional_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Factory_Patrón_creacional_
+{
+    internal class FabricaProductor
+    {
+        public static FabricaAbstracta getFactory(String tipoFabrica)
+        {
+
+            if (tipoFabrica.Equals("BD"))
+            {
+                return new ConexionFabrica();
+
+            }
+            else if (tipoFabrica.Equals("REST"))
+            {
+                return new ConexionRESTFabrica();
+            }
+
+            return null;
+        }
+    }
+}
--- Abstract Factory(Patrón creacional)/Program.cs
+++ Abstract Factory(Patrón creacional)/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Factory__Patrón_creacional_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,15 @@ namespace Abstract_Factory_Patrón_creacional_
     {
         static void Main(string[] args)
         {
+            FabricaAbstracta fa = FabricaProductor.getFactory("BD");
+            IConexion cxBD1 = fa.getBD("MYSQL");
 
+            cxBD1.Conectar();
+
+            FabricaAbstracta faREST = FabricaProductor.getFactory("REST");
+            IConexionREST cxRS1 = faREST.getREST("COMPRAS");
+
+            cxRS1.leerURL("https://www.youtube.com/");
         }
     }
 }
