0b77eb876cfe4d0376df45ef06ab8ed77d4096e9
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-03 21:54:18 -0300

    Aplicando patron de diseño prototype

---
+++ Prototype (Patrón creacional)/CuentaAHImpl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype__Patrón_creacional_
+{
+    internal class CuentaAHImpl : ICuenta
+    {
+        private string tipo;
+        private double monto;
+
+        public CuentaAHImpl()
+        {
+            tipo = "AHORRO";
+        }
+        public ICuenta Clonar()
+        {
+            CuentaAHImpl cuenta = null;
+
+            try
+            {
+                cuenta = (CuentaAHImpl)MemberwiseClone();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            return cuenta;
+        }
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string toString()
+        {
+            return "CuentaAHImpl [tipo= " + tipo + ", monto= " + monto + "]";
+        }
+        public void setMonto(double monto) { this.monto = monto; }
+        public double getMonto() { return monto; }
+    }
+}
--- Prototype (Patrón creacional)/Program.cs
+++ Prototype (Patrón creacional)/Program.cs
@@ -10,7 +10,20 @@ namespace Prototype__Patrón_creacional_
     {
         static void Main(string[] args)
         {
+            CuentaAHImpl cuentaAhorro = new CuentaAHImpl();
+            cuentaAhorro.setMonto(200);
+            CuentaAHImpl cuentaClonada = (CuentaAHImpl)cuentaAhorro.Clonar();
 
+            if (cuentaClonada != null)
+            {
+                //Aca lo que vemos es que tiene los mismos 
+                //valores que la clase cuentaAhorro.
+                Console.WriteLine(cuentaClonada.toString()); 
+            }
+            //Aca vemos que cuentaClonada y cuentaAhorro
+            //no ocupan el mismo espacio en memoria.
+            Console.WriteLine(cuentaClonada == cuentaAhorro);
+            Console.ReadKey();
         }
     }
 }
