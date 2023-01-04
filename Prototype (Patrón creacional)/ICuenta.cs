2e4e02f26559f91c8629eabd9ee19b113510d3d5
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-03 21:32:17 -0300

    proyecto prototype

---
+++ Prototype (Patrón creacional)/ICuenta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype__Patrón_creacional_
+{
+    internal interface ICuenta
+    {
+    }
+}
