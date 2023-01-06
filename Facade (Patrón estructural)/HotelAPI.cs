cf1ee252b7d48c18eb0080b91eb19bd65b2d4faf
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-05 23:02:11 -0300

    Clase HotelApi

---
+++ Facade (Patrón estructural)/HotelAPI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade__Patrón_estructural_
+{
+    internal class HotelAPI
+    {
+        public void BuscarVuelos(string fechaIda, string fechaVuelta, string origen, string destino)
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine("Vuelos encontrads para " + destino + " desde " + origen);
+            Console.WriteLine("Fecha Ida: " + fechaIda + ". Fecha Vuelta: " + fechaVuelta);
+            Console.WriteLine("===================================");
+        }
+    }
+}
