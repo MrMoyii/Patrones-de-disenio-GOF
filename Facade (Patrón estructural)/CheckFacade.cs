39cd428f15badb7c81036099952bbd172a55bc0a
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-05 23:09:36 -0300

    Clase CheckFacade

---
+++ Facade (Patrón estructural)/CheckFacade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade__Patrón_estructural_
+{
+    internal class CheckFacade
+    {
+        private AvionAPI avionAPI;
+        private HotelAPI hotelAPI;
+
+        public CheckFacade()
+        {
+            avionAPI= new AvionAPI();
+            hotelAPI= new HotelAPI();
+        }
+
+        public void buscar(string fechaIda, string fechaVuelta, string origen, string destino)
+        {
+            avionAPI.BuscarVuelos(fechaIda,fechaVuelta,origen,destino);
+            hotelAPI.BuscarHoteles(fechaIda,fechaVuelta,origen,destino);
+        }
+    }
+}
