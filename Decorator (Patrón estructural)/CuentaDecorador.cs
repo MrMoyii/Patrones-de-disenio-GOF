35d8007e6ba18b483a1074ac2cba35b847a1e9e5
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-07 01:31:24 -0300

    CuentaDecorador

---
+++ Decorator (Patrón estructural)/CuentaDecorador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator__Patrón_estructural_
+{
+    internal class CuentaDecorador : ICuentaBancaria
+    {
+        protected ICuentaBancaria cuentaDecorada;
+
+        public CuentaDecorador(ICuentaBancaria cuentaDecorada)
+        {
+            this.cuentaDecorada = cuentaDecorada;
+        }
+
+        public void abrirCuenta(Cuenta c)
+        {
+            this.cuentaDecorada.abrirCuenta(c);
+        }
+    }
+}
