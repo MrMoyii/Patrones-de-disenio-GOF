cec7773038cae57b2d2e3eedce77d72dd46fc0a6
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-07 01:20:42 -0300

    Interface ICuentaBancaria

---
+++ Decorator (Patrón estructural)/ICuentaBancaria.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator__Patrón_estructural_
+{
+    internal interface ICuentaBancaria
+    {
+        void abrirCuenta(Cuenta c);
+    }
+}
