0aa9084ca11e17b851303c63ca2ba40e805b93e9
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-09 10:38:06 -0300

    Interface IGatoCompañero

---
+++ Factory (ej con gatos) (Patrón creacional)/IGatoCompañero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory__ej_con_gatos___Patrón_creacional_
+{
+    internal interface IGatoCompañero
+    {
+        void jugar();
+        void alimentar();
+    }
+}
