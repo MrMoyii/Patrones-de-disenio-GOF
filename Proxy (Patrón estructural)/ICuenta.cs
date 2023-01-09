e6b14db13fa1b3ba1bb02a4ac91fb82756d39a34
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-08 23:47:01 -0300

    Interface ICuenta

---
+++ Proxy (Patrón estructural)/ICuenta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy__Patrón_estructural_
+{
+    internal interface ICuenta
+    {
+        Cuenta retirarDinero(Cuenta cuenta, double monto);
+        Cuenta depositarDinero(Cuenta cuenta, double monto);
+        void mostrarSaldo(Cuenta cuenta);
+    }
+}
