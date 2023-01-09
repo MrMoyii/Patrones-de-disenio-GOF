9135d61a427a0119cc381c1ad3c41885b0da1b5d
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-08 23:53:35 -0300

    Clase CuentaBancoAImpl

---
+++ Proxy (Patrón estructural)/CuentaBancoAImpl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy__Patrón_estructural_
+{
+    internal class CuentaBancoAImpl : ICuenta
+    {
+        public Cuenta retirarDinero(Cuenta cuenta, double monto)
+        {
+            double saldoActual = cuenta.getSaldoInicial() - monto;
+            cuenta.setSaldoInicial(saldoActual);
+            Console.WriteLine("Saldo actual: " + cuenta.getSaldoInicial());
+            return cuenta;
+        }
+        public Cuenta depositarDinero(Cuenta cuenta, double monto)
+        {
+            double saldoActual = cuenta.getSaldoInicial() + monto;
+            cuenta.setSaldoInicial(saldoActual);
+            Console.WriteLine("Saldo actual: " + cuenta.getSaldoInicial());
+            return cuenta;
+        }
+
+        public void mostrarSaldo(Cuenta cuenta)
+        {
+            Console.WriteLine("Saldo actual: " + cuenta.getSaldoInicial());
+        }
+
+    }
+}
