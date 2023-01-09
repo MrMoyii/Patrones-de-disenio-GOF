95a80f754b59d9e818b0c3b0fe399421adbaafe4
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-09 10:45:14 -0300

    Fabrica de Gatos

---
+++ Factory (ej con gatos) (Patrón creacional)/FabricaDeGatos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory__ej_con_gatos___Patrón_creacional_
+{
+    internal class FabricaDeGatos
+    {
+        public IGatoCompañero Crear()
+        {
+            /*Aca puedo descomentar el codigo y obtener otro tipo de gato*/
+            //return new GatoJugueton();
+
+            return new GatoComilon();
+        }
+    }
+}
