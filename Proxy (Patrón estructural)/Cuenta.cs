441c2ab859b3647e2ac8dae69a42302bd9dfe892
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-08 23:46:18 -0300

    Clase Cuenta

---
+++ Proxy (Patrón estructural)/Cuenta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy__Patrón_estructural_
+{
+    internal class Cuenta
+    {
+        private int idCuenta;
+        private string usuario;
+        private double saldoInicial;
+
+        public Cuenta(int idCuenta, string usuario, double saldoInicial)
+        {
+            this.idCuenta = idCuenta;
+            this.usuario = usuario;
+            this.saldoInicial = saldoInicial;
+        }
+        public int getIdCuenta() { return idCuenta; }
+        public void setIdCuenta(int idCuenta)
+        {
+            this.idCuenta=idCuenta;
+        }
+        public string getUsuario() { return usuario; }
+        public void setUsuario(string usuario)
+        {
+            this.usuario = usuario;
+        }
+        public double getSaldoInicial() { return saldoInicial; }
+        public void setSaldoInicial(double saldoInicial)
+        {
+            this.saldoInicial=saldoInicial;
+        }
+    }
+}
