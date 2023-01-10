8f95f2839ae8a33b82d2a75a458e30520439557e
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 17:13:39 -0300

    Clase Caretaker

---
+++ Memento (Patrón de comportamiento)/Caretaker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento__Patrón_de_comportamiento_
+{
+    internal class Caretaker
+    {
+        private List<Memento> mementos = new List<Memento>();
+
+        public void addMemento(Memento m)
+        {
+            mementos.Add(m);
+        }
+
+        public Memento getMemento(int index)
+        {
+            return mementos[index];
+        }
+    }
+}
