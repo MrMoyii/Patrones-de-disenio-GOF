9f5469b5e66a5cf7bb9e8ef40db942a48b185096
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 17:02:14 -0300

    Clase Juego

---
+++ Memento (Patrón de comportamiento)/Juego.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento__Patrón_de_comportamiento_
+{
+    internal class Juego
+    {
+        private string nombre;
+        private int checkpoint;
+
+        public int getCheckpoint()
+        {
+            return checkpoint;
+        }
+        public void setCheckpoint(int checkpoint)
+        {
+            this.checkpoint = checkpoint;
+        }
+
+        public string getNombre()
+        {
+            return nombre;
+        }
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string toString()
+        {
+            return "Juego [nombre=" + nombre + ", checkpoint= " + checkpoint + "]";
+        }
+    }
+}
