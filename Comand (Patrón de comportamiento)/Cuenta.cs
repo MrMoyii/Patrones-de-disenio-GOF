95134c61199396ae451e56b518d9dd285c3a564d
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 15:02:43 -0300

    Patron Comand y Clase Cuenta de este

---
+++ Comand (Patrón de comportamiento)/Cuenta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comand__Patrón_de_comportamiento_
+{
+    internal class Cuenta
+    {
+        private int id;
+        private double saldo;
+
+        public Cuenta(int id, double saldo)
+        {
+            this.id = id;
+            this.saldo = saldo;
+        }
+
+        public void retirar(double monto)
+        {
+            this.saldo -= monto;
+            Console.WriteLine("[COMANDO RETIRAR] Cuenta: " + id + " Saldo: " + this.saldo);
+        }
+
+        public void depositar(double monto)
+        {
+            this.saldo += monto;
+            Console.WriteLine("[COMANDO DEPOSITAR] Cuenta: " + id + " Saldo: " + this.saldo);
+        }
+    }
+}
