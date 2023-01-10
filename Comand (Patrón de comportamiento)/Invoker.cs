67ab1b2fec09d13984861f61488e74351a34d88d
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 15:15:28 -0300

    Clase Invoker

---
+++ Comand (Patrón de comportamiento)/Invoker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comand__Patrón_de_comportamiento_
+{
+    internal class Invoker
+    {
+        private List<IOperacion> operaciones= new List<IOperacion>();
+
+        public void recibirOperacion(IOperacion operacion)
+        {
+            this.operaciones.Add(operacion);
+        }
+        public void realizarOperacion()
+        {
+            this.operaciones.ForEach(operacion => { operacion.execute(); });
+        }
+    }
+}
