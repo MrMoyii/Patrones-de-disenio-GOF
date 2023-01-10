b33dfeed4501152e6ce49f062e402a5142412b2e
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 17:07:55 -0300

    Clases Memento y Originator

---
+++ Memento (Patrón de comportamiento)/Memento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento__Patrón_de_comportamiento_
+{
+    internal class Memento
+    {
+        private Juego estado;
+        public Memento(Juego estado)
+        {
+            this.estado = estado;
+        }
+        public Juego getEstado() { return estado; }
+    }
+}
---
+++ Memento (Patrón de comportamiento)/Originator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento__Patrón_de_comportamiento_
+{
+    internal class Originator
+    {
+        private Juego estado;
+
+        public void setEstado(Juego estado)
+        {
+            this.estado = estado;
+        }
+        public Juego getEstado() { return estado; }
+
+        public Memento guardar()
+        {
+            return new Memento(estado);
+        }
+        public void restaurar(Memento m)
+        {
+            this.estado = m.getEstado();
+        }
+    }
+}
