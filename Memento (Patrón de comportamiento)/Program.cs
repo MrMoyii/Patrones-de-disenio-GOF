357b4e77cba64b0ff5e9ac90008547e64e714292
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 16:44:56 -0300

    Proyecto Memento, documentacion

---
+++ Memento (Patrón de comportamiento)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento__Patrón_de_comportamiento_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Memento es un patrón de diseño de comportamiento que
+              permite guardar y restaurar el estado previo de un objeto
+              sin revelar los detalles de su implementación.*/
+        }
+    }
+}
