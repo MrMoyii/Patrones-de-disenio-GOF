ae7e10107a5192019613d23608d31c237dc789aa
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 15:08:57 -0300

    Clase DepositarImpl

---
+++ Comand (Patrón de comportamiento)/DepositarImpl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comand__Patrón_de_comportamiento_
+{
+    internal class DepositarImpl : IOperacion
+    {
+        private Cuenta cuenta;
+        private double monto;
+
+        public DepositarImpl(Cuenta cuenta, double monto)
+        {
+            this.cuenta = cuenta;
+            this.monto = monto;
+        }
+
+        public void execute()
+        {
+            this.cuenta.depositar(this.monto);
+        }
+    }
+}
