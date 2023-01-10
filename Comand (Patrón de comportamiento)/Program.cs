d3c0a325c982f7d6bcc2484285481c1ec3b3315b
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-10 15:21:04 -0300

    Implementacion del patron

--- Comand (Patrón de comportamiento)/Program.cs
+++ Comand (Patrón de comportamiento)/Program.cs
@@ -10,7 +10,18 @@ namespace Comand__Patrón_de_comportamiento_
     {
         static void Main(string[] args)
         {
+            Cuenta cuenta = new Cuenta(1, 200);
 
+            DepositarImpl opDepositar = new DepositarImpl(cuenta, 100); //Comando depositar
+            RetirarImpl opRetirar = new RetirarImpl(cuenta, 50); //Comando retirar
+
+            Invoker ivk = new Invoker();
+            ivk.recibirOperacion(opDepositar);
+            ivk.recibirOperacion(opRetirar);
+
+            ivk.realizarOperacion();
+
+            Console.ReadKey();
         }
     }
 }
