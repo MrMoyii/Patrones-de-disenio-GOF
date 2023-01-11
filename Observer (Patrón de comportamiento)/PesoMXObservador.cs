fa3eaa138c3564fa39da621d99c7d8bf043b9af0
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:25:06 -0300

    Clase PesoMXObservador

---
+++ Observer (Patrón de comportamiento)/PesoMXObservador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer__Patrón_de_comportamiento_
+{
+    internal class PesoMXObservador : Observador
+    {
+        private double valorCambio = 19.07;
+
+        public PesoMXObservador(Subject sujeto)
+        {
+            this.sujeto = sujeto;
+            this.sujeto.agregar(this);
+        }
+
+        public override void actualizar()
+        {
+            Console.WriteLine("MX: " + (sujeto.getEstado() * valorCambio));
+        }
+    }
+}
