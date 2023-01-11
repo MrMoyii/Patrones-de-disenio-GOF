171edf39bfa867814d4f26028dbba5f78a123775
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:20:03 -0300

    Clase subject y clase abstracta Observador

---
+++ Observer (Patrón de comportamiento)/Observador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer__Patrón_de_comportamiento_
+{
+    internal abstract class Observador
+    {
+        protected Subject sujeto;
+        public abstract void actualizar();
+    }
+}
---
+++ Observer (Patrón de comportamiento)/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer__Patrón_de_comportamiento_
+{
+    internal class Subject
+    {
+        private List<Observador> observadores = new List<Observador>();
+        private int estado;
+
+        public int getEstado()
+        {
+            return estado;
+        }
+
+        public void setEstado(int estado)
+        {
+            this.estado = estado;
+            notificarTodosObservadores();
+        }
+
+        public void agregar(Observador observador)
+        {
+            observadores.Add(observador);
+        } 
+
+        public void notificarTodosObservadores()
+        {
+            observadores.ForEach(o => o.actualizar());
+        }
+    }
+}
