8751e2f98222ca132347d1cdf74c2231a19ca09f
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:26:22 -0300

    Clase PesoARGObservador

---
+++ Observer (Patrón de comportamiento)/PesoARGObservador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer__Patrón_de_comportamiento_
+{
+    internal class PesoARGObservador : Observador
+    {
+        private double valorCambio = 29.86;
+
+        public PesoARGObservador(Subject sujeto)
+        {
+            this.sujeto = sujeto;
+            this.sujeto.agregar(this);
+        }
+
+        public override void actualizar()
+        {
+            Console.WriteLine("ARG: " + (sujeto.getEstado() * valorCambio));
+        }
+    }
+}
