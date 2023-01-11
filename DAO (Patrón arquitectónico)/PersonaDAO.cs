0deee23abd21d13284f3bf10a8b62999b9b98f8b
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 16:04:22 -0300

    Interface PersonaDAO

---
+++ DAO (Patrón arquitectónico)/PersonaDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO__Patrón_arquitectónico_
+{
+    internal interface PersonaDAO
+    {
+        List<Persona> listarTodos();
+        Persona leerPorId(int id);
+        void registrar(Persona persona);
+        void actualizar(Persona persona);
+        void eliminar(int id);
+    }
+}
