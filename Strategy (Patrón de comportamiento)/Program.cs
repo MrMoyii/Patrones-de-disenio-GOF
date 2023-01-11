4aa7cec003170feab17c559130b12ccd735e613f
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:38:10 -0300

    Documentacion

--- Strategy (Patrón de comportamiento)/Program.cs
+++ Strategy (Patrón de comportamiento)/Program.cs
@@ -10,6 +10,9 @@ namespace Strategy__Patrón_de_comportamiento_
     {
         static void Main(string[] args)
         {
+            /*Strategy es un patrón de diseño de comportamiento que te permite 
+              definir una familia de algoritmos, colocar cada uno de ellos 
+              en una clase separada y hacer sus objetos intercambiables.*/
         }
     }
 }
