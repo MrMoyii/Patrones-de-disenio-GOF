f798a417005e20568de9feb8d53a40ea00203b6e
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:35:42 -0300

    Proyecto Strategy, interface IEstrategia

---
+++ Strategy (Patrón de comportamiento)/IEstrategia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy__Patrón_de_comportamiento_
+{
+    internal interface IEstrategia
+    {
+        void analizar();
+    }
+}
