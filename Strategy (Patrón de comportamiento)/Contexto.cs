0380a26d2ad1a6407130819ce563606477b7c13d
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 15:01:56 -0300

    Clase Contexto

---
+++ Strategy (Patrón de comportamiento)/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy__Patrón_de_comportamiento_
+{
+    internal class Contexto
+    {
+        private IEstrategia estrategia;
+        public Contexto(IEstrategia estrategia)
+        {
+            this.estrategia = estrategia;
+        }
+
+        public void Ejecutar()
+        {
+            this.estrategia.analizar();
+        }
+    }
+}
