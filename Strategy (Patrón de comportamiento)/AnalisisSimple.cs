54c2a5f15b7a153ca1d18747cc39c99d3035256e
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:43:07 -0300

    Clase AnalisisSimple

---
+++ Strategy (Patrón de comportamiento)/AnalisisSimple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy__Patrón_de_comportamiento_
+{
+    internal abstract class AnalisisSimple : IEstrategia
+    {
+        public void analizar()
+        {
+            Iniciar();
+            SaltarZip();
+            Detener();
+        }
+
+        public abstract void Iniciar();
+        public abstract void SaltarZip();
+        public abstract void Detener();
+    }
+}
