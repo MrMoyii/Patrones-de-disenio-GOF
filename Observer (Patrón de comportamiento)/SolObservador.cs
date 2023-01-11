8047571b9da766eee0561a1e6911085993d869e2
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:23:29 -0300

    Clase SolObservador

---
+++ Observer (Patrón de comportamiento)/SolObservador.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer__Patrón_de_comportamiento_
+{
+    internal class SolObservador : Observador
+    {
+
+        private double valorCambio = 3.25;
+
+        public SolObservador(Subject sujeto)
+        {
+            this.sujeto = sujeto;
+            this.sujeto.agregar(this);
+        }
+
+        public override void actualizar()
+        {
+            Console.WriteLine("PEN: " + (sujeto.getEstado() * valorCambio));
+        }
+    }
+}
