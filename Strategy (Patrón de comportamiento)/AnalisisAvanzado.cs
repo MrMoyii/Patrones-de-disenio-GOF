340575cab5cf0dfdf1438559095e87ffbc380d92
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:45:17 -0300

    Clase AnalisisAvanzado

---
+++ Strategy (Patrón de comportamiento)/AnalisisAvanzado.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy__Patrón_de_comportamiento_
+{
+    internal abstract class AnalisisAvanzado : IEstrategia
+    {
+        public void analizar()
+        {
+            Iniciar();
+            AnalizarMemoria();
+            AnalizarKeyLoggers();
+            AnalizarRootKits();
+            DescomprimirZip();
+            Detener();
+        }
+
+        public abstract void Iniciar();
+        public abstract void AnalizarMemoria();
+        public abstract void AnalizarKeyLoggers();
+        public abstract void AnalizarRootKits();
+        public abstract void DescomprimirZip();
+        public abstract void Detener();
+    }
+}
