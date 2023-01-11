99c70b92ba992be6c63cd2922e437d682acabeca
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-11 14:54:09 -0300

    Clase AntivirusSimple

---
+++ Strategy (Patrón de comportamiento)/AntivirusSimple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Strategy__Patrón_de_comportamiento_
+{
+    internal class AntivirusSimple : AnalisisSimple
+    {
+        public override void Iniciar()
+        {
+            Console.WriteLine("Antivirus Simple - Análisis simple iniciado");
+        }
+
+        public override void SaltarZip()
+        {
+            try
+            {
+                Console.WriteLine("Analizando...");
+                Thread.Sleep(2500);
+                Console.WriteLine("No se pudo analizar archivos con extension '.zip");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+        }
+
+        public override void Detener()
+        {
+            Console.WriteLine("Antivirus Simple - Análisis simple finalizado");
+        }
+    }
+    }
+}
