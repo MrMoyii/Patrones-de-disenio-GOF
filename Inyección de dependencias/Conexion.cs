2f497a50dc0668fef8a1a233457baa566f4ab8c4
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-12 10:18:39 -0300

    Clase Conexion

---
+++ Inyección de dependencias/Conexion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inyección_de_dependencias
+{
+    internal class Conexion
+    {
+        private string usuario;
+        private string clave;
+        private string host;
+
+        public Conexion(string usuario, string clave, string host)
+        {
+            this.usuario = usuario;
+            this.clave = clave;
+            this.host = host;
+        }
+    }
+}
