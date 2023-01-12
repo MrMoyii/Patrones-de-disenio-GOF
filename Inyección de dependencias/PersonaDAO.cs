19d736f2e73df42762cc275c0fce81ce32658f75
Omar Moyano <omarmoyano7w@gmail.com>
2023-01-12 10:17:04 -0300

    Base para el ejemplo

---
+++ Inyección de dependencias/PersonaDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO__Patrón_arquitectónico_
+{
+    internal interface PersonaDAO : CRUD<Persona>
+    {
+        void mostrarNombre();
+    }
+}
---
+++ Inyección de dependencias/PersonaDAOImpl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO__Patrón_arquitectónico_
+{
+    internal class PersonaDAOImpl : PersonaDAO
+    {
+        public List<Persona> listarTodos()
+        {
+            List<Persona> lista = new List<Persona>();
+            Persona per = new Persona();
+            per.setID(1);
+            per.setNombres("Moyi");
+
+            lista.Add(per);
+
+            per = new Persona();
+            per.setID(2);
+            per.setNombres("Code");
+
+            lista.Add(per);
+
+            return lista;
+        }
+
+        public Persona leerPorId(int id)
+        {
+            //Logica
+            //Esto esta muy mal
+            return null;
+        }
+
+        public void registrar(Persona persona)
+        {
+            Console.WriteLine("Persona " + persona.getNombres() + " registrada");
+        }
+
+        public void actualizar(Persona persona)
+        {
+            Console.WriteLine("Persona " + persona.getNombres() + " actualizada");
+        }
+
+        public void eliminar(int id)
+        {
+            Console.WriteLine("id " + id + " eliminado");
+        }
+
+        public void mostrarNombre(Persona persona)
+        {
+            Console.WriteLine("Nombre de la persona: " + persona.getNombres());
+        }
+    }
+}
